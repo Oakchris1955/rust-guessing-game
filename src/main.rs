603ced4270e663235cea4ce146dd380308024f3e
Oakchris1955 <80592203+Oakchris1955@users.noreply.github.com>
2022-06-18 14:55:20 +0300

    Renamed some variables

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io;
 use std::io::Write;
 
+// Include module to randomly select number
 use rand::Rng;
 
 fn get_user_input(msg: &str) -> u32 {
@@ -18,7 +19,7 @@ fn get_user_input(msg: &str) -> u32 {
 
 fn main() {
     // Edit these variables to modify the game
-    let remaining_tries = 4;
+    let total_tries = 4;
     let max_number = 100; // the number to iter last
     let min_number = 1; // the number to begin from
 
@@ -31,12 +32,12 @@ fn main() {
 
     println!("A simple guessing game. Made by Oakchris1955");
 
-    for current_guess in 1..remaining_tries+1 {
-        // println!("Number of current guess: {current_guess}");
+    for current_try in 1..total_tries+1 {
+        // println!("Number of current guess: {current_try}");
 
-        if current_guess == remaining_tries {
+        if current_try == total_tries {
             println!("Last try. ");
-        } else if current_guess == 1 {
+        } else if current_try == 1 {
             println!("Alright, let's begin. ");
         } else {
             println!("Let's retry");
@@ -46,18 +47,18 @@ fn main() {
 
         // Check if found the correct number
         if guess == secret_number {
-            println!("Congratulations. You found the secret number after {} guesses", current_guess);
+            println!("Congratulations. You found the secret number after {} guesses", current_try);
             break;
         }
 
-        if current_guess!=remaining_tries {
+        if current_try!=total_tries {
             println!("I'm sorry, but your guess wasn't correct.");
             if guess < secret_number {
                 println!("Next time try a higher number");
             } else {
                 println!("Next time try a lower number");
             }
-            println!("You have {} tries remaining", remaining_tries - current_guess);
+            println!("You have {} tries remaining", total_tries - current_try);
         } else {
             println!("I'm sorry, you lost. The secret number was {secret_number}");
         }
